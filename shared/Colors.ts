e1bf3f5d6adfe02758d1fcc07695cec7326a2931
romain <rpanaget@yoobic.com>
2019-12-28 02:14:32 +0100

    feat(shared): added shared colors and styles

---
+++ shared/Colors.ts
@@ -0,0 +1,8 @@
+const Colors = {
+  accent: "#00CCCC",
+  grey: "#333333",
+  lightGrey: "#888888",
+  veryLightGrey: "#EEEEEE"
+};
+
+export default Colors;
---
+++ shared/Styles.ts
@@ -0,0 +1,7 @@
+import Colors from "./Colors";
+
+export const SeparatorStyle = {
+  height: 1,
+  marginHorizontal: 16,
+  backgroundColor: Colors.veryLightGrey
+};
