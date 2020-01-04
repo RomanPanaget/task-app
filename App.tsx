4911193b8f9637aefd974dde29e9d15db11f086b
romain <rpanaget@yoobic.com>
2020-01-04 18:52:45 +0100

    feat(app): added redux persistance

--- App.tsx
+++ App.tsx
@@ -3,13 +3,24 @@ import MainStackNavigator from "./navigation/MainStackNavigator";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import todoReducer from "./redux/TodoListReducer";
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import { AsyncStorage } from "react-native";
 
-const store = createStore(todoReducer);
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage
+};
+const persistedReducer = persistReducer(persistConfig, todoReducer);
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
 
 export default function App() {
   return (
     <Provider store={store}>
-      <MainStackNavigator />
+      <PersistGate loading={null} persistor={persistor}>
+        <MainStackNavigator />
+      </PersistGate>
     </Provider>
   );
 }
