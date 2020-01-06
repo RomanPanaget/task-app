49bca0f30e9913f39b5c154629bec49667ca04c8
romain <rpanaget@yoobic.com>
2020-01-06 09:46:01 +0000

    feat(components): changed the checkbox looking

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -1,40 +1,30 @@
 import React from "react";
 import { View, StyleSheet, TouchableWithoutFeedback } from "react-native";
 import Colors from "../shared/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const SIZE = 24;
-const GAP = 4;
-const BORDER_WIDTH = 2;
-const OUTER_BORDER_RADIUS = 12;
-const INNER_BORDER_RADIUS = OUTER_BORDER_RADIUS - GAP - BORDER_WIDTH;
+const SIZE_UNCHECKED = 28;
 
 const Checkbox = props => {
   return (
     <TouchableWithoutFeedback onPress={props.onPress}>
-      <View style={styles.default}>
-        <View style={props.checked ? styles.checked : {}} />
+      <View
+        style={{
+          width: SIZE,
+          height: SIZE,
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Ionicons
+          name={props.checked ? "ios-checkbox-outline" : "ios-square-outline"}
+          size={props.checked ? SIZE : SIZE_UNCHECKED}
+          color={Colors.accent}
+        />
       </View>
     </TouchableWithoutFeedback>
   );
 };
-
-const styles = StyleSheet.create({
-  default: {
-    width: SIZE,
-    height: SIZE,
-    borderRadius: OUTER_BORDER_RADIUS,
-    borderColor: Colors.accent,
-    borderWidth: BORDER_WIDTH,
-    backgroundColor: "#FFF",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  checked: {
-    width: SIZE - 2 * GAP - 2 * BORDER_WIDTH,
-    height: SIZE - 2 * GAP - 2 * BORDER_WIDTH,
-    backgroundColor: Colors.accent,
-    borderRadius: INNER_BORDER_RADIUS
-  }
-});
 
 export default Checkbox;
